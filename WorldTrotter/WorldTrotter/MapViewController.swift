9902e05865d6b94cbabf476a6156dc80b274e861
shoh <dhtkdgh429@google.com>
2019-03-21 00:29:33 +0900

    MapView 현재 위치 표시 추가

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -12,9 +12,16 @@ import MapKit
 class MapViewController: UIViewController {
     
     private var mapView: MKMapView!
+    private var locationManager = CLLocationManager()
+    
+    private var currentLocationButton: UIButton!
+    private var zoomInButton: UIButton!
+    private var zoomOutButton: UIButton!
+    
+//    private var lastLocation: MKUserLocation?
     
     override func loadView() {
-        print("Map View loading!!")
+        print("Map View load!!")
         
         // 지도 뷰 생성
         mapView = MKMapView()
@@ -22,6 +29,74 @@ class MapViewController: UIViewController {
         // 지도 뷰를 이 뷰컨트롤러의 view로 설정...
         view = mapView
         
+        // 위치 이벤트 매니저
+        
+        locationManager.delegate = self
+        // private. 사용자 승인 요청.
+        locationManager.requestWhenInUseAuthorization()
+        // 정확도 설정.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // 위치 업데이트 시작.
+        locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        // 현재위치 버튼.
+        currentLocationButton = UIButton(type: .custom)
+        currentLocationButton.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        currentLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        currentLocationButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        currentLocationButton.setTitleColor(UIColor.black, for: .normal)
+        currentLocationButton.setTitle("현재 위치", for: .normal)
+        currentLocationButton.addTarget(self, action: #selector(updateUserLocation), for: .touchUpInside)
+        view.addSubview(currentLocationButton!)
+        // 버튼 width = 스크린 넓이/2 - layout 마진 8.0 (11버전 이상에서는 20)
+        var screenWidth = UIScreen.main.bounds.width / 2 - 8.0
+        if #available(iOS 11, *) {
+            screenWidth = UIScreen.main.bounds.width / 2 - 20.0
+        }
+        let currentBtnLeading = currentLocationButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
+        let currentBtnBottom = currentLocationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10.0)
+        let currentBtnWidth = currentLocationButton.widthAnchor.constraint(equalToConstant: screenWidth)
+        currentBtnLeading.isActive = true
+        currentBtnBottom.isActive = true
+        currentBtnWidth.isActive = true
+        
+        // 줌 인 버튼..
+        zoomInButton = UIButton(type: .custom)
+        zoomInButton.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        zoomInButton.translatesAutoresizingMaskIntoConstraints = false
+        zoomInButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        zoomInButton.setTitleColor(UIColor.black, for: .normal)
+        zoomInButton.setTitle("+", for: .normal)
+        view.addSubview(zoomInButton!)
+        
+        let zoomInBtnLeading = zoomInButton.leadingAnchor.constraint(equalTo: currentLocationButton.trailingAnchor, constant: 5.0)
+        let zoomInBtnBottom = zoomInButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10.0)
+        zoomInBtnLeading.isActive = true
+        zoomInBtnBottom.isActive = true
+        
+        // 줌 아웃 버튼..
+        zoomOutButton = UIButton(type: .custom)
+        zoomOutButton.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        zoomOutButton.translatesAutoresizingMaskIntoConstraints = false
+        zoomOutButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        zoomOutButton.setTitleColor(UIColor.black, for: .normal)
+        zoomOutButton.setTitle("-", for: .normal)
+        view.addSubview(zoomOutButton)
+        
+        let zoomOutBtnLeading = zoomOutButton.leadingAnchor.constraint(equalTo: zoomInButton.trailingAnchor, constant: 5.0)
+        let zoomOutBtnTrailing = zoomOutButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        let zoomOutBtnBottom = zoomOutButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10.0)
+        let zoomOutBtnWidth = zoomOutButton.widthAnchor.constraint(equalTo: zoomInButton.widthAnchor, multiplier: 1.0)
+        zoomOutBtnLeading.isActive = true
+        zoomOutBtnTrailing.isActive = true
+        zoomOutBtnBottom.isActive = true
+        zoomOutBtnWidth.isActive = true
+        
+        // 맵 컨트롤 버튼 추가하기.
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex = 0
@@ -38,8 +113,26 @@ class MapViewController: UIViewController {
         topConstraint.isActive = true
         leadingConstraint.isActive = true
         trailingConstraint.isActive = true
+        
+        
     }
-
+    
+    @objc func updateUserLocation() {
+        var region = MKCoordinateRegion()
+        let userCoord = mapView.userLocation.coordinate
+        
+        //Zoom 설정
+        region.span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        //위치(경도,위도) 설정
+        let coordinate = CLLocationCoordinate2D(latitude: userCoord.latitude, longitude: userCoord.longitude)
+        //center 설정
+        region.center = coordinate
+        mapView.setRegion(region, animated: true)
+        print("update")
+        
+    }
+    
+    // 맵 컨트롤 버튼 액션
     @objc func mapTypeChanged(segControl: UISegmentedControl) {
         switch segControl.selectedSegmentIndex {
         case 0:
@@ -55,6 +148,26 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         print("Map View did load!!")
-        
+    }
+}
+
+extension MapViewController : MKMapViewDelegate {
+    //Adjusting the zoom
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+//        self.lastLocation = userLocation
+        self.updateUserLocation()
+    }
+    
+    func mapViewWillStartLocatingUser(_ mapView: MKMapView) {
+        print("starting locating")
+    }
+    func mapViewDidStopLocatingUser(_ mapView: MKMapView) {
+        print("stop locating")
+    }
+}
+
+extension MapViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        debugPrint("receive location")
     }
 }
