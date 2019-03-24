dbe15bfb1427878000f9636ea899eba7768e0eb1
shoh <dhtkdgh429@google.com>
2019-03-24 18:10:27 +0900

    quiz spring animation

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     // View
     @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet weak var currentQuestionLabelCenterX: NSLayoutConstraint!
     @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet weak var nextQuestionLabelCenterX: NSLayoutConstraint!
+    
     @IBOutlet var answerLabel: UILabel!
     
     // Model
@@ -28,6 +31,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         currentQuestionLabel.text = questions[currentQuestionIndex]
+        updateOffScreenLabel()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,14 +41,40 @@ class ViewController: UIViewController {
         self.nextQuestionLabel.alpha = 0
     }
     
+    private func updateOffScreenLabel() {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterX.constant = -screenWidth
+    }
+    
     private func animateLabelTransitions() {
+        
+        // 아직 처리하지 않은 레이아웃의 변경 요청.
+        view.layoutIfNeeded()
+        
         // 알파 값을 변경..
-        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
-            self.currentQuestionLabel.alpha = 0
-            self.nextQuestionLabel.alpha = 1
+        // + center x 조건 변경
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterX.constant = 0
+        self.currentQuestionLabelCenterX.constant += screenWidth
+        
+        // spring animation..
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 0.5,
+                       options: .curveEaseOut,
+                       animations: {
+                        self.currentQuestionLabel.alpha = 0
+                        self.nextQuestionLabel.alpha = 1
+                        
+                        self.view.layoutIfNeeded()
         }) { _ in
             swap(&self.currentQuestionLabel,
                  &self.nextQuestionLabel)
+            swap(&self.currentQuestionLabelCenterX,
+                 &self.nextQuestionLabelCenterX)
+            
+            self.updateOffScreenLabel()
         }
     }
     
